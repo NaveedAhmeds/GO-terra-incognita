eeb6814a1088e56353f5cce77eec927f6a8bf414
Naveed Ahmed Syed <syednaveed2006@icloud.com>
2026-03-28 19:51:54 -0400

    feat: Basic Structs

---
+++ Structs/structs.go
@@ -0,0 +1,56 @@
+// Structs in GO!!
+// -> Key value pairs similarities.
+// -> Struct Keys can hold any type
+//
+//
+
+package main
+
+import "fmt"
+
+type messageToSend struct {
+	message     string
+	phoneNumber int
+}
+
+func test(m messageToSend) {
+	fmt.Printf("Sending message as: '%s' to: '%v'", m.message, m.phoneNumber)
+}
+
+// key exmaple...
+type car struct {
+	Make       string
+	Model      string
+	FrontWheel Wheel
+	BackWheel  Wheel
+}
+
+type Wheel struct {
+	Radius   int
+	Material string
+}
+
+
+// ------ Anonymous Structs in GO -----
+//  -> just like a normal struct but is defined without a name and therefore cannot be referenced elsewhere in the code.
+//  -> best use case is to prevent from re-using a struct definition you never intended to use 
+//	Note: best coding practice to use general structs rather than the Anonymous struct
+myCar := struct{
+	Make string
+	Model string
+}{
+	Make: "Mercedes",
+	Model: "AMG"
+}
+
+
+func main() {
+	test(messageToSend{
+		message:     "Naveed",
+		phoneNumber: 1234567890,
+	})
+
+	// Example use case of the holding key mutation.
+	myCar := car{}
+	myCar.FrontWheel.Radius = 5
+}
